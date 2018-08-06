3ea8279a5b4054752b59e175dd72bbefbee645e5
Elvis Souza Dias <elvissouza1@live.com>
2018-08-05 23:18:07 -0300

    Protótipo da função de Listas todos os índices com Fill Factor menor que X

---
+++ src/model/bean/FillFactor.java
@@ -0,0 +1,19 @@
+package model.bean;
+
+/**
+ *
+ * @author elvis
+ */
+public class FillFactor {
+    private int valorFillFactor;
+
+    public int getValorFillFactor() {
+        return valorFillFactor;
+    }
+
+    public void setValorFillFactor(int valorFillFactor) {
+        this.valorFillFactor = valorFillFactor;
+    }
+    
+    
+}
